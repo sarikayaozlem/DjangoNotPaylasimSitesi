a16548d03cb99982c528f402d071f3530d91af97
sarikayaozlem <yamac201218@gmail.com>
2020-03-29 01:56:47 +0300

    Pycharm #2 Category-Notes

--- product/admin.py
+++ product/admin.py
@@ -1,11 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
-from product.models import Category
+from product.models import Category, Note
 
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'status']
     list_filter = ['status']
 
+class NoteAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'status']
+    list_filter = ['status','category']
+
+
 admin.site.register(Category,CategoryAdmin)
+admin.site.register(Note,NoteAdmin)
--- product/models.py
+++ product/models.py
@@ -7,13 +7,33 @@ class Category(models.Model):
         ('True', 'Evet'),
         ('False', 'Hayır'),
     )
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=75)
     keywords = models.CharField(max_length=255)
     description = models.CharField(max_length=255)
     image = models.ImageField(blank=True, upload_to='images/')
     status = models.CharField(max_length=10, choices=STATUS)
     slug = models.SlugField()
     parent = models.ForeignKey('self', blank=True, null=True, related_name='children', on_delete=models.CASCADE)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+
+class Note(models.Model):
+    STATUS = (
+        ('True', 'Evet'),
+        ('False', 'Hayır'),
+    )
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=75)
+    keywords = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    image = models.ImageField(blank=True, upload_to='images/')
+    detail = models.TextField()
+    status = models.CharField(max_length=10, choices=STATUS)
     create_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
 
